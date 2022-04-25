062130ed1b137519a7b946066aeb9876bba17a2f
Xtend Ettisaf <98172341+XtendEttisaf@users.noreply.github.com>
2022-04-25 14:50:12 +0600

    Add files via upload

--- java.js
+++ java.js
@@ -1,2 +1,8 @@
-var name = prompt("তোমার নাম তা বল:");
-document.getElementById('wishName').innerHTML = name + " কে 'ইত্তিসাফ রূপ' এর পক্ষ থেকে \n &hearts; ঈদ মোবারাক &hearts;"
+var name = prompt("তোমার নামটা বল (বাংলায়):");
+
+if (name=="") {
+    document.getElementById('wishName').innerHTML ="যদি নাম যে না বলিস তাহলে কি Jonny বলে উইশ করবো?"
+} else {
+    document.getElementById('wishName').innerHTML = name + " কে 'ইত্তিসাফ রূপ' এর পক্ষ থেকে \n &hearts; ঈদ মোবারাক &hearts;"
+
+}
